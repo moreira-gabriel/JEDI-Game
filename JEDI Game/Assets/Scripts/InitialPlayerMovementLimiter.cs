157a0a7986fdc42d2486d69896e74c3d0b916ee0
Eric-Catarina <2020954200@teiacoltec.org>
2022-10-12 21:16:27 -0300

    Refactoring: D -> State Machine and single animation functions
    Troquei a maquina de estados e as funções individuais por uma única função chamada PlayNextAnimation() para reduzir o número de linhas e facilitar a leitura

--- JEDI Game/Assets/Scripts/InitialPlayerMovementLimiter.cs
+++ JEDI Game/Assets/Scripts/InitialPlayerMovementLimiter.cs
@@ -21,90 +21,46 @@ public class InitialPlayerMovementLimiter : MonoBehaviour
 
         private bool HandlingPlayerInput;
 
-    // Simple State Machine to manage the player Movement and animations
-    public void ChangeState(GameState nextState){
-
-        switch(nextState){
-            case GameState.Starting:
-                PlayFirstAnimation();
-                break;
-            case GameState.FirstStep:
-                HandleFirstMovement();
-                break;
-            case GameState.SecondStep:
-                HandleSecondMovement();
-                break;
-            case GameState.ThirdStep:
-                HandleThirdMovement();
-                break;
-
-            //Do this Until the game needs to move to the next phase (EndOfLevel)
-
-            case GameState.EndOfLevel:
-                HandleLoadNextScene();
-                break;
+        private void PlayFirstAnimation(){
+            // PLAY THE FIRST ANIMATION/CUTSCENE
+            HandlingPlayerInput = true;
         }
-    }
 
-    private void PlayFirstAnimation(){
-        HandlingPlayerInput = false;
-
-        // PLAY THE FIRST CUTSCENE
-        ChangeState(GameState.FirstStep);
-
-        HandlingPlayerInput = true;
-    }
-    private void HandleFirstPlayerInput(){
-        HandlingPlayerInput = true;
-
-        Debug.Log("Starting");
-        HandlingPlayerInput = false;
-        // Play First Animation/Cutscene ()
-    }
-    private void HandleFirstMovement(){
-        ChangeState(GameState.SecondStep);
-                Debug.Log("First Movement");
-        // Play Second Animation
-    }
-    private void HandleSecondMovement(){
-        ChangeState(GameState.ThirdStep);
-                Debug.Log("Second Movement");
-        // Play Third Animation
-    }
-    private void HandleThirdMovement(){
-        ChangeState(GameState.EndOfLevel);
-                Debug.Log("Third Movement");
-        // Play Fourth Animation
-    }
-    private void HandleLoadNextScene(){
-                Debug.Log("Changing Scenes...");
-        //Load the next phase
-    }
-
+        private void PlayNextAnimation(){
+            HandlingPlayerInput = false;
+            // Play the current Animation and make assign the next Animation
+            HandlingPlayerInput = true;
+        }
     private void HandlePlayerInput(){
         switch(CurrentValidArrowInput){
             case ArrowInput.Right:
-                if(Input.GetKeyDown(KeyCode.RightArrow));
+                if(Input.GetKeyDown(KeyCode.RightArrow)){
+                    PlayNextAnimation();
+                }
                 break;
             case ArrowInput.Down:
-                if(Input.GetKeyDown(KeyCode.DownArrow));
+                if(Input.GetKeyDown(KeyCode.DownArrow)){
+                    PlayNextAnimation();
+                }
                 break;
             case ArrowInput.Left:
-                if(Input.GetKeyDown(KeyCode.LeftArrow));
+                if(Input.GetKeyDown(KeyCode.LeftArrow)){
+                    PlayNextAnimation();
+                }
                 break;
             case ArrowInput.Up:
-                if(Input.GetKeyDown(KeyCode.UpArrow));
+                if(Input.GetKeyDown(KeyCode.UpArrow)){
+                    PlayNextAnimation();
+                }
                 break;
         }
     }
-
     void Start() {
-        ChangeState(GameState.Starting);
         CurrentValidArrowInput = ArrowInput.Right;
         HandlingPlayerInput = false;
+        PlayFirstAnimation();
     } 
     
-
     void Update()
     {
         if(HandlingPlayerInput){
