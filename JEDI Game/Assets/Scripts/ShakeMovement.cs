305842aa654d17d458b160147291669ac21e78d4
Eric-Catarina <2020954200@teiacoltec.org>
2022-10-15 21:07:57 -0300

    fix: All three balls shaking correctly

--- JEDI Game/Assets/Scripts/ShakeMovement.cs
+++ JEDI Game/Assets/Scripts/ShakeMovement.cs
@@ -4,46 +4,34 @@ using UnityEngine;
 
 public class ShakeMovement : MonoBehaviour
 {
-    public float speedX = 10f;
-    public float speedY = 10f;
-
-    [SerializeField]
-    private enum Axis{
-        Horizontal,
-        Vertical,
-        Diagonal
-    }
+    public float speedX = 1f;
+    public float speedY = 1f;
 
     private Vector2 movement;
+  
     private Rigidbody2D rb;
 
     private float timer;
 
     [SerializeField]
     private float timeToReverseMovement = 0.5f;
-
     private int direction = 1;
-
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        movement = new Vector2(speedX, speedY);
     }
     void Update()
     {
         timer += Time.deltaTime;
         if(timer >= timeToReverseMovement){
             timer = 0;
-            direction = direction * -1;
+            direction *= -1;
         }
-
     }
 
     private void FixedUpdate(){
-        movement = new Vector2(speedX * direction, speedY);
-        rb.velocity = movement * speedX;
+       rb.velocity = movement * direction;
     }
-
-    
-
 }
